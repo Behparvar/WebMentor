cebbd4e1e1f4bf01762d281850dc451605c2fe8b
alikaali100 <alikaramouzian@icloud.com>
2025-04-24 14:57:15 +0330

    feat: create base templates and page layouts

--- WebMentor/Training/admin.py
+++ WebMentor/Training/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, TrainingContent
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
+@admin.register(TrainingContent)
+class TrainingContentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'created_at']
+    list_filter = ['category']
+    search_fields = ['title', 'description']
+    fields = ['title', 'category', 'video_path', 'demo_link', 'description'] 
--- WebMentor/Training/views.py
+++ WebMentor/Training/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, TrainingContent
 
 def category_list(request):
     categories = Category.objects.all()
-    return render(request, 'training/category_list.html', {'categories': categories})
+    return render(request, 'training/base.html', {
+        'categories': categories,
+        'page_type': 'list',
+    })
 
 def category_detail(request, slug):
-    category = Category.objects.get(slug=slug)
+    category = get_object_or_404(Category, slug=slug)
     contents = TrainingContent.objects.filter(category=category)
-    return render(request, 'training/category_detail.html', {
+    return render(request, 'training/base.html', {
         'category': category,
         'contents': contents,
+        'categories': Category.objects.all(),
+        'page_type': 'detail',
     })
