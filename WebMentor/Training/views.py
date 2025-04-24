cb1c857ae8727513c47c87931d0da80cecf81f57
alikaali100 <alikaramouzian@icloud.com>
2025-04-24 13:49:23 +0330

    feat: add views and configure URLs

--- WebMentor/Training/views.py
+++ WebMentor/Training/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Category, TrainingContent
 
-# Create your views here.
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'training/category_list.html', {'categories': categories})
+
+def category_detail(request, slug):
+    category = Category.objects.get(slug=slug)
+    contents = TrainingContent.objects.filter(category=category)
+    return render(request, 'training/category_detail.html', {
+        'category': category,
+        'contents': contents,
+    })
